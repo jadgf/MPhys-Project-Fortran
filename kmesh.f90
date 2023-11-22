a03f2f3926d68b652a8015f0942bf5b73dc65836
jamesballaro <jamesalexballaro@gmail.com>
2023-11-22 01:48:13 +0000

    magnetization and orbital projections implemented

--- kmesh.f90
+++ kmesh.f90
@@ -2,23 +2,25 @@ Program Wannier_band_structure
     Implicit None
 !--------to be midified by the usere
     character(len=80):: prefix="BiTeI"
-    integer,parameter::nkpath=3,np=100,meshres=10
-    real*8,parameter::ef= 4.18903772,ikmax=0.1, tolerance = 0.01
+    integer,parameter::nkpath=3,np=100,meshres=110
+    real*8,parameter::ef= 4.18903772,ikmax=0.1, tolerance = 0.005, energy_diff = 0.02
 !------------------------------------------------------
-    real*8 kz, kmesh(3,meshres**2), dx, dy, CBM, m_x, m_y, m_z
+    real*8 kmesh(3,meshres**2), dx, dy, CBM, m_x, m_y, m_z, L_x, L_y, L_z
     character(len=30)::klabel(nkpath)
     character(len=80) hamil_file,nnkp,line
     integer*4,parameter::nk=(nkpath-1)*np+1
     integer*4 i,j,k,nr,i1,i2,nb,lwork,info
     integer*8 count
-    real*8,parameter::third=1d0/3d0!,kz=0d0
+    real*8,parameter::third=1d0/3d0, two = 2.0d0, sqrt2 = sqrt(two)
     real*8 phase,pi2,jk,a,b
     real*8 klist(3,1:nk),xk(nk),kpath(3,np),avec(3,3),bvec(3,3),ktemp1(3),ktemp2(3),xkl(nkpath),rvec(3),kvec(3)
-    real*8,allocatable:: rvec_data(:,:),ene(:,:),rwork(:),k_ene(:),kpoints(:,:), m_k_data(:,:)
+    real*8,allocatable:: rvec_data(:,:),ene(:,:),rwork(:),k_ene(:),kpoints(:,:), m_k_data(:,:),  L_k_data(:,:)
     integer*4,allocatable:: ndeg(:)
     complex*16,allocatable:: Hk(:,:),Hamr(:,:,:),work(:),kp_eivec(:,:,:),H_col(:)
-    complex*16 temp1,temp2,chi_k(2,1),m_x_comp(1,1), m_y_comp(1,1), m_z_comp(1,1)
-    complex*8 pauli_x(2, 2), pauli_y(2, 2), pauli_z(2, 2)
+    complex*16 chi_k(2,1),m_x_comp(1,1), m_y_comp(1,1), m_z_comp(1,1), L_x_comp(1,1), L_y_comp(1,1),&
+                L_z_comp(1,1), phi_k(3,1)
+    complex*8 pauli_x(2, 2), pauli_y(2, 2), pauli_z(2, 2), Lx(3,3), Ly(3,3), Lz(3,3), Y_lm(3,1)
+    complex*8, parameter:: one = complex(1.d0,0.d0),im = complex(0.d0,1.d0), zero = complex(0.d0,0.d0)
 !------------------------------------------------------
     write(hamil_file,'(a,a)')trim(adjustl(prefix)),"_hr_topological.dat"
     write(nnkp,'(a,a)')      trim(adjustl(prefix)),".nnkp"
@@ -26,7 +28,7 @@ Program Wannier_band_structure
     pi2=4.0d0*atan(1.0d0)*2.0d0
 
 !---------------  reciprocal vectors
-    open(98,file=trim(adjustl(nnkp)))
+    open(98,file=trim(adjustl(nnkp)),err=333)
 111 read(98,'(a)')line
     if(trim(adjustl(line)).ne."begin real_lattice") goto 111
     read(98,*)avec
@@ -63,11 +65,11 @@ Program Wannier_band_structure
     ktemp2=klist(1,nk)*bvec(:,1)+klist(2,nk)*bvec(:,2)+klist(3,nk)*bvec(:,3)
     xk(nk)=xk(nk-1)+sqrt(dot_product(ktemp2-ktemp1,ktemp2-ktemp1))
     xkl(nkpath)=xk(nk)
-!      write(*,*)klist
+    !      write(*,*)klist
     klist=klist*pi2
 
 !------read H(R)
-    open(99,file=trim(adjustl(hamil_file)))
+    open(99,file=trim(adjustl(hamil_file)),err=444)
     open(100,file='kmesh.dat')
     read(99,*)
     read(99,*)nb,nr
@@ -85,7 +87,7 @@ Program Wannier_band_structure
    lwork=max(1,2*nb-1)
    allocate(work(max(1,lwork)),rwork(max(1,3*nb-2)))
 
-!---- Fourier transform H(R) to H(k)
+!----- Fourier transform H(R) to H(k)
     ene=0d0
     do k=1,nk
        HK=(0d0,0d0)
@@ -125,7 +127,9 @@ Program Wannier_band_structure
     enddo
     
 !----- Perform cartesian fourier transform
-    count = 0
+    allocate(kpoints(2,400), kp_eivec(400,nb,nb), m_k_data(3,400),  L_k_data(3,400))
+
+    count=1
     k_ene = 0d0
     do k=1,meshres**2
         HK=(0d0,0d0)
@@ -137,50 +141,31 @@ Program Wannier_band_structure
         enddo
         call zheev('V','U',nb,HK,nb,k_ene,work,lwork,rwork,info)
 
-        if((ABS(k_ene(13) - (CBM+0.02)).lt.tolerance).or.(ABS(k_ene(14) - (CBM+0.02)).lt.tolerance)) then
-            count = count + 1
-        endif
-    enddo
-    print *, "Number of intersections: ", count
-    allocate(kpoints(2,count), kp_eivec(count,nb,nb), m_k_data(3,count))
-    count=1
-    do k=1,meshres**2
-        HK=(0d0,0d0)
-        kvec = kmesh(1,k)*bvec(:,1) + kmesh(2,k)*bvec(:,2) + kmesh(3,k)*bvec(:,3)
-        do i=1,nr
-            rvec = rvec_data(1,i)*avec(:,1) + rvec_data(2,i)*avec(:,2) + rvec_data(3,i)*avec(:,3)
-            phase = rvec(1)*kvec(1) + rvec(2)*kvec(2) + rvec(3)*kvec(3)
-            HK=HK+Hamr(:,:,i)*dcmplx(cos(phase),-sin(phase))/float(ndeg(i))
-        enddo
-        call zheev('V','U',nb,HK,nb,k_ene,work,lwork,rwork,info)
-
-        if((ABS(k_ene(13) - (CBM+0.02)).lt.tolerance).or.(ABS(k_ene(14) - (CBM+0.02)).lt.tolerance)) then
+        if((ABS(k_ene(13) - (CBM+energy_diff)).lt.tolerance).or.(ABS(k_ene(14) - (CBM+energy_diff)).lt.tolerance)) then
             kpoints(:,count) = kvec(:)
             kp_eivec(count,:,:) = Hk(:,:)
             count = count + 1
         endif
     enddo
+    print *, "Number of intersections: ", count-1
 
 !-----Spin projection
+   !--Define Pauli matrices
     pauli_x(1, 1) = complex(0.d0, 0.d0); pauli_x(1, 2) = complex(1.d0, 0.d0)
         pauli_x(2, 1) = complex(1.d0, 0.d0); pauli_x(2, 2) = complex(0.d0, 0.d0)
     pauli_y(1, 1) = complex(0.d0, 0.d0); pauli_y(1, 2) = complex(0.d0, -1.d0)
         pauli_y(2, 1) = complex(0.d0, 1.d0); pauli_y(2, 2) = complex(0.d0, 0.d0)
     pauli_z(1, 1) = complex(1.d0, 0.d0); pauli_z(1, 2) = complex(0.d0, 0.d0)
         pauli_z(2, 1) = complex(0.d0, 0.d0); pauli_z(2, 2) = complex(-1.d0, 0.d0)
-
+   !----Defined
     do i=1, count-1
-        do j=1, nb
-            m_x_comp = 0d0
-            m_y_comp = 0d0
-            m_z_comp = 0d0
+        ! do j=1, nb
             m_x = 0d0
             m_y = 0d0
             m_z = 0d0
-            H_col = kp_eivec(i,:,j)
+            H_col = kp_eivec(i,:,13) !----CHECK THIS '13'
             do k=1, 9
                 chi_k = reshape([H_col(k), H_col(k+9)], [2, 1])
-
                 m_x_comp = matmul(conjg(transpose(chi_k)),matmul(pauli_x, chi_k))
                 m_y_comp = matmul(conjg(transpose(chi_k)),matmul(pauli_y, chi_k))
                 m_z_comp = matmul(conjg(transpose(chi_k)),matmul(pauli_z, chi_k))
@@ -189,19 +174,57 @@ Program Wannier_band_structure
                 m_y = m_y + real(m_y_comp(1,1))
                 m_z = m_z + real(m_z_comp(1,1))
             enddo
+        ! enddo
+        m_k_data(:,i) = [m_x, m_y, m_z]
+    enddo
+
+!-----L projection
+   !-Define angular momentum Operators 
+    Lx(1, 1) = zero; Lx(1, 2) = one; Lx(1,3) = zero
+    Lx(2, 1) = one; Lx(2, 2) = zero; Lx(2,3) = one
+    Lx(3, 1) = zero; Lx(3, 2) = one; Lx(3,3) = zero
+
+    Ly(1, 1) = zero; Ly(1, 2) = -im; Ly(1,3) = zero
+    Ly(2, 1) = im; Ly(2, 2) = zero; Ly(2,3) = -im
+    Ly(3, 1) = zero; Ly(3, 2) = im; Ly(3,3) = zero
+
+    Lz(1, 1) = one; Lz(1, 2) = zero; Lz(1,3) = zero
+    Lz(2, 1) = zero; Lz(2, 2) = zero; Lz(2,3) = zero
+    Lz(3, 1) = zero; Lz(3, 2) = zero; Lz(3,3) = -one
+
+    Lx = Lx*1/sqrt2
+    Ly = Ly*1/sqrt2
+
+   !-Defined
+    do i=1, count-1
+        L_x = 0d0
+        L_y = 0d0
+        L_z = 0d0
+        H_col = kp_eivec(i,:,13)
+        do k=1, 6
+            phi_k = reshape([H_col((k-1)*3+1), H_col((k-1)*3+2),H_col((k-1)*3+3)], [3, 1])
+
+            Y_lm(1,1) = (-sqrt2/2) * (phi_k(1,1) + im*phi_k(2,1));
+            Y_lm(2,1) = (sqrt2/2) * (phi_k(1,1) - im*phi_k(2,1));
+            Y_lm(3,1) = phi_k(3,1); 
+
+            L_x_comp = matmul(conjg(transpose(Y_lm)),matmul(Lx, Y_lm))
+            L_y_comp = matmul(conjg(transpose(Y_lm)),matmul(Ly, Y_lm))
+            L_z_comp = matmul(conjg(transpose(Y_lm)),matmul(Lz, Y_lm))
+            
+            L_x = L_x + real(L_x_comp(1,1))
+            L_y = L_y + real(L_y_comp(1,1))
+            L_z = L_z + real(L_z_comp(1,1))
         enddo
-        m_k_data(1,i) = m_x
-        m_k_data(2,i) = m_y
-        m_k_data(3,i) = m_z
-        print *, m_x, m_y, m_z
+        L_k_data(:,i) = [L_x, L_y, L_z]
     enddo
     
-
 !-----Write to kmesh.dat
     do i=1, count
-        write(100,'(5(f12.6))') kpoints(1,i), kpoints(2,i), m_k_data(1,count), m_k_data(2,count), m_k_data(3,count)
+        write(100,'(8(f12.6))') kpoints(1,i), kpoints(2,i), m_k_data(1,i)/30, m_k_data(2,i)/30, m_k_data(3,i)/30,&
+                                L_k_data(1,i)/20, L_k_data(2,i)/20, L_k_data(3,i)/20
     enddo
-    call write_plt(nkpath,xkl,klabel,ef)
+    call write_plt(energy_diff)
     stop
 333   write(*,'(3a)')'ERROR: input file "',trim(adjustl(nnkp)),' not found'
     stop
@@ -210,35 +233,39 @@ Program Wannier_band_structure
 
 end
 
-   subroutine write_plt(nkp, xkl, kl, ef)
+!-----kmesh.plt subroutine
+   subroutine write_plt(energy_diff)
     implicit none
-    integer, intent(in) :: nkp
-    real*8, intent(in) :: xkl(nkp), ef
-    character(len=30), intent(in) :: kl(nkp)
-    integer :: i
-
+    real*8, intent(in) :: energy_diff
+    real*8 e_converted
+    e_converted =1000* energy_diff
     open(99, file='kmesh.plt')
-
+    
     write(99, '(a)') 'set xrange [-0.1 : 0.1]'
-    write(99, '(a)') 'set terminal pdfcairo enhanced font "DejaVu" transparent fontscale 1 size 5.00in, 7.50in'
+    write(99, '(a)') 'set title "2D k-mesh plot at 20meV above CBM"'
+    write(99, '(a)') 'set terminal pdfcairo enhanced font "DejaVu" transparent fontscale 1 size 5.00in, 5.00in'
     write(99, '(a)') 'set output "kmesh.pdf"'
-    write(99, '(12(a,/),a)') &
+    write(99, '(15(a,/),a)') &
         'set encoding iso_8859_1', &
         'set size ratio 0 1.0,1.0', &
-        'set ylabel "$k_{y}$ coordinate"', &
+        'set ylabel "k_y coordinate"', &
+        'set xlabel "k_x coordinate"', &
         'set yrange [ -0.1 : 0.1 ]', &
         'unset key', &
-        'set ytics 1.0 scale 1 nomirror out', &
+        'set ytics 0.05 scale 1 nomirror out', &
         'set mytics 2', &
         'set parametric', &
         'set trange [-10:10]', &
         'set multiplot', &
         'plot "kmesh.dat" u 1:2 with points lt 1 lw 3', &
-        'plot "kmesh.dat" using 1:2:3:4 with vectors head filled size screen 0.03,15,45 lc variable', &
+        '#For Spin Projection: (use # to comment out either one)', &
+        'plot "kmesh.dat" using 1:2:3:4 with vectors head filled lt 1 lc rgb "blue"', &
+        '#For Angular Momentum Projection:', &
+        'plot "kmesh.dat" using 1:2:6:7 with vectors head filled lt 1 lc rgb "blue"', &
         'unset multiplot'
 
     close(99)
-end subroutine write_plt
+   end subroutine write_plt
 
-! ! ------ gfortran -o bandplot wanr2k.f90 -lblas -llapack
+! ! ------ gfortran -o kmesh kmesh.f90 -lblas -llapack
 ! gfortran -o kplot  kmesh.f90 -LC:/msys64/ucrt64/lib/lapack-3.11.0/build/lib -llapack -lblas
