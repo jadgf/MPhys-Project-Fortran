5270920b0fe57de948950b58eb99abf87872be42
jadgf <felixhayes1@gmail.com>
2023-12-03 18:23:01 +0000

    update files

--- pt-interpolate.f90
+++ pt-interpolate.f90
@@ -1,9 +1,9 @@
 module parameters
     Implicit None
-!--------to be midified by the usere
+!--------to be modified by the user
     character(len=80):: prefix="BiTeI"
-    real*8,parameter::ef= 4.18903772,kmax=0.1,kzmax=0.2,alpha=0.77474747,tolerance=0.03
-    integer,parameter::meshres=30,zmeshres=30,nkpoints=(2*meshres+1),nkzpoints=(2*zmeshres+1),nbmin=12,nbmax=13
+    real*8,parameter::ef= 4.18903772,kmax=0.2,kzmax=0.2,alpha=0.77474747,tolerance=0.02
+    integer,parameter::meshres=40,zmeshres=40,nkpoints=(2*meshres+1),nkzpoints=(2*zmeshres+1),nbmin=12,nbmax=13
     integer nb
     
 end module parameters
@@ -42,7 +42,7 @@ Program Projected_band_structure
 !------read H(R)
     open(99,file=trim(adjustl(top_file)))
     open(97,file=trim(adjustl(triv_file)))
-    open(100,file='3denergy.csv')
+    open(100,file='3denergy.dx')
     read(99,*)
     read(99,*)nb,nr
     allocate(rvec_data(3,nr),rvec_data_t(3,nr),Hk(nb,nb),top_Hr(nb,nb,nr),triv_Hr(nb,nb,nr),ndeg(nr),ene(nb))
@@ -66,46 +66,41 @@ Program Projected_band_structure
 	dz=kzmax/zmeshres
 
 !----- Create header of dx files
-!   write(100, '(a,3(1x,i8))') 'object 1 class gridpositions counts',nkpoints,nkpoints,nkzpoints
-!   write(100, '(a,3(1x,f12.6))') 'origin',-kmax,-kmax,-kzmax
-!   write(100, '(a,3(1x,f12.6))') 'delta',dxy,0d0,0d0
-!   write(100, '(a,3(1x,f12.6))') 'delta',0d0,dxy,0d0
-!   write(100, '(a,3(1x,f12.6))') 'delta',0d0,0d0,dz
-!   write(100, '(a,3(1x,i8))') 'object 2 class gridconnections counts',nkpoints,nkpoints,nkzpoints
-!   write(100, '(a,i8,a,i10,a)') 'object 3 class array type float rank 1 shape',nbmax-nbmin+1,&
-!                                    ' item', nkpoints,' data follows'
+    write(100, '(a,3(1x,i8))') 'object 1 class gridpositions counts',nkpoints,nkpoints,nkzpoints
+    write(100, '(a,3(1x,f12.6))') 'origin',-kmax,-kmax,-kzmax
+    write(100, '(a,3(1x,f12.6))') 'delta',dxy,0d0,0d0
+    write(100, '(a,3(1x,f12.6))') 'delta',0d0,dxy,0d0
+    write(100, '(a,3(1x,f12.6))') 'delta',0d0,0d0,dz
+    write(100, '(a,3(1x,i8))') 'object 2 class gridconnections counts',nkpoints,nkpoints,nkzpoints
+    write(100, '(a,i8,a,i10,a)') 'object 3 class array type float rank 1 shape',nbmax-nbmin+1,&
+                                     ' item', nkpoints*nkpoints*nkzpoints,' data follows'
 
 !----- Perform fourier transform
-    allocate(sam(3,nbmin:nbmax), oam(3,nbmin:nbmax), k_ene(nb), k_ene_data(nkpoints*nkpoints*nkzpoints,2))
-	allocate(kmesh(nkpoints*nkpoints*nkzpoints,3))
+    allocate(sam(3,nbmin:nbmax), oam(3,nbmin:nbmax), k_ene(nb))
 
-	count=0
 	do ikx=-meshres,meshres
 		do iky=-meshres,meshres
 			do ikz=-zmeshres,zmeshres
-				count=count+1
-				kpoint(1)= ikx*dxy
-				kpoint(2)= iky*dxy
-				kpoint(3)= ikz*dz
+				kpoint(1)=ikx*dxy
+				kpoint(2)=iky*dxy
+				kpoint(3)=ikz*dz 
 
 				do i=1,nr
 					rvec = rvec_data(1,i)*avec(:,1) + rvec_data(2,i)*avec(:,2) + rvec_data(3,i)*avec(:,3)
 					
 					phase = dot_product(kpoint,rvec)
-					HK=HK+((1-alpha)*(triv_Hr(:,:,i))+(alpha)*(top_Hr(:,:,i)))*dcmplx(cos(phase),-sin(phase))/float(ndeg(i))
-					!HK=HK+(top_Hr(:,:,i)*dcmplx(cos(phase),-sin(phase))/float(ndeg(i)))
+					!HK=HK+((1-alpha)*(triv_Hr(:,:,i))+(alpha)*(top_Hr(:,:,i)))*dcmplx(cos(phase),-sin(phase))/float(ndeg(i))
+					HK=HK+(top_Hr(:,:,i)*dcmplx(cos(phase),-sin(phase))/float(ndeg(i)))
 				enddo
 				call zheev('V','U',nb,HK,nb,k_ene,work,lwork,rwork,info)
-				if(k_ene(nbmax) - k_ene(nbmin) < tolerance) then
-					write(100, '((1x,f12.6),a,(1x,f12.6),a,(1x,f12.6))') kpoint(1), ',', kpoint(2), ',', kpoint(3) 
-				endif
+				write(100, '(2(1x,f12.6))') k_ene(nbmin), k_ene(nbmax) 
 			enddo
 		enddo
 	enddo
-!   write(100,'(A,/,A,/,A,/,A)') &
-!   'object "regular positions regular connections" class field', &
-!   'component "positions" value 1', &
-!   'component "connections" value 2', &
-!   'component "data" value 3', &
-!   'end'
+    write(100,'(A,/,A,/,A,/,A)') &
+    'object "regular positions regular connections" class field', &
+    'component "positions" value 1', &
+    'component "connections" value 2', &
+    'component "data" value 3', &
+    'end'
 end Program Projected_band_structure
